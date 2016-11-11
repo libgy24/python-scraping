4e0cd2bd2053326cbe376f96f961f8e12b5366c2
libgy24 <lizhengyizak@gmail.com>
2016-11-11 10:11:53 -0600

    simple modification to demonstrate git

--- chapter1/1-basicExample.py
+++ chapter1/1-basicExample.py
@@ -2,3 +2,4 @@ from urllib.request import urlopen
 #Retrieve HTML string from the URL
 html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
 print(html.read())
+print(html.read())
